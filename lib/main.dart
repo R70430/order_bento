adedc2802aea8ab9e1fe43b7c9ab039678d9c5a5
Clyde_Cho <clyde_cho@mail.chungyo.net>
2022-09-26 18:12:34 +0800

    初始化

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(middle: Text("Order Bento !"),),
+      child: Center(
+
+        child: CupertinoButton(
+          onPressed: () {},
+          child: const Text("登入"),
+        ),
+      ),
+    );
+  }
+}
